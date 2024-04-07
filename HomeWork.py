ad241488b4aa06f24eb7e73738fbf6f9abea37ae
Alex007-Biz <bizukin@yandex.ru>
2024-04-07 13:31:18 +0300

    9

--- HomeWork.py
+++ HomeWork.py
@@ -1,30 +1,36 @@
 from abc import ABC, abstractmethod
 class Weapon(ABC):
     @abstractmethod
-    def attack(self, weapon):
-        self.weapon_type = weapon
+    def attack(self):
         pass
 class Fighter():
-    def __init__(self, name, weapon):
+    def __init__(self, name, weapon: Weapon):
         self.name = name
         self.weapon = weapon
-    def change_weapon(self, weapon):
+    def change_weapon(self, weapon: Weapon):
+        self.weapon = weapon
         print(f"смена оружия на {weapon}")
+
+    def fight(self):
+        print(self.weapon.attack())
+
+
 class Monster():
     pass
 
 class Sword(Weapon):
-    def attack(self, weapon):
+    def attack(self):
         print("Атака мечом")
 
 class Bow(Weapon):
-    def attack(self, weapon):
+    def attack(self):
         print("Выстрел из лука")
 
-jason = Fighter("Jason", "sword")
+sword1 = Sword()
+bow1 = Bow()
+jason = Fighter("Jason", sword1)
 jason.change_weapon("sword")
 
 jason.change_weapon("bow")
+sword1.attack()
 
-bob = Bow("bow")
-bob.attack("bow")
